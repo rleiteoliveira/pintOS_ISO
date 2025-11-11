dafe51830296584d47f1508f91ce9cb9460beb3a
rleiteoliveira <rlo3@cin.ufpe.br>
2025-11-11 15:43:27 -0300

    Projeto 3

---
+++ Projeto 3/src/userprog/syscall.h
@@ -0,0 +1,10 @@
+#ifndef USERPROG_SYSCALL_H
+#define USERPROG_SYSCALL_H
+
+#include "threads/synch.h"
+
+void syscall_init (void);
+
+extern struct lock filesys_lock;
+
+#endif /* userprog/syscall.h */
